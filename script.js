6b611f6c33d773e3083eff8edb73bbc64ea45a2d
Matt Mullen <mhmullen@icloud.com>
2023-11-20 17:20:03 -0800

    debugging color function and date

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
   $(".saveBtn").on("click", function () { //listener for click events on the save button.
     console.log($(this).siblings("textarea").val());
@@ -16,20 +14,27 @@ $(function () {
   });
 
   $(".time-block").each(function (index) {
-    console.log($(this));
-    
-
+    console.log($(this).attr("id"));
+    var thisHour = $(this).attr("id").split("-")[1]
+    // console.log(thisHour);
+    // console.log(dayjs().hour());
+    if (parseInt(thisHour) < dayjs().hour) {
+      $(this).addClass("past");
+      $(this).removeClass("present")
+      $(this).removeClass("future");
+    }
+    if (parseInt(thisHour) = dayjs().hour) {
+      $(this).addClass("present");
+      $(this).removeClass("past")
+      $(this).removeClass("future");
+    }
+    if (parseInt(thisHour) > dayjs().hour) {
+      $(this).addClass("future");
+      $(this).removeClass("present")
+      $(this).removeClass("past");
+    }
   });
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
 
   $("#currentDay").text(dayjs().format("dddd MMM DD, YYYY"))
 });
+
